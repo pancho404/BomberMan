a287fa15c58216a86154ff61ee4830d4c711295b
pancho404 <franciscosanchez2101@gmail.com>
2022-06-14 09:31:20 -0300

    Deprecated "BOOM".

--- Assets/Scripts/BombExplosion.cs
+++ Assets/Scripts/BombExplosion.cs
@@ -12,8 +12,7 @@ public class BombExplosion : MonoBehaviour
        
         if (countdown<=0)
         {
-            Debug.Log("BOOM");
-            FindObjectOfType<MapDestroyer>().Explode(transform.position);
+            FindObjectOfType<MapDestroyer>().Explode(transform.position);            
             Destroy(gameObject);
         }
     }
